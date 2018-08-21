b5f8cfa495c49d12ae4ea3ed663f5d987e8dd3cb
iamdoubz <4871781+iamdoubz@users.noreply.github.com>
2018-08-21 13:15:45 -0500

    Update admin.php
    Incremental changes.

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@
 OCP\User::checkAdminUser();
 
 $tmpl = new OCP\Template('shorten', 'admin');
+
 $tmpl->assign('type', OC::$server->getConfig()->getAppValue('shorten', 'type', ''));
 $tmpl->assign('host', OC::$server->getConfig()->getAppValue('shorten', 'host', ''));
 $tmpl->assign('api', OC::$server->getConfig()->getAppValue('shorten', 'api', ''));
