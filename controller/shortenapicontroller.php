5a3548b2b42acfdc35686b78a55001844e4ecf08
iamdoubz <4871781+iamdoubz@users.noreply.github.com>
2018-08-21 11:59:36 -0500

    Update shortenapicontroller.php
    Change author and reduce whitespace.

--- controller/shortenapicontroller.php
+++ controller/shortenapicontroller.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * ownCloud - shorten
+ * Nextcloud - shorten
  *
  * This file is licensed under the Affero General Public License version 3 or
  * later. See the COPYING file.
  *
- * @author Ben Curtis <ownclouddev@nosolutions.com>
- * @copyright Ben Curtis 2015
+ * @author iamdoubz
+ * @copyright iamdoubz 2018
  */
 
 namespace OCA\Shorten\Controller;
@@ -20,34 +20,21 @@ use \OCP\IRequest;
 #\OCP\User::checkLoggedIn();
 \OCP\App::checkAppEnabled('shorten');
 
-
 class ShortenApiController extends ApiController {
-
     private $userId;
-
     public function __construct($appName, IRequest $request){
         parent::__construct($appName, $request);
     }
-
-
-    /**
-     * @NoAdminRequired
-     */
+    /* @NoAdminRequired */
     public function makeurl() {
 	require_once 'shorten/lib/makeurl.php';
 	return generateUrl();
     }
-
-    /**
-     * @NoAdminRequired
-     */
+    /* @NoAdminRequired */
     public function code() {
 	require_once 'shorten/lib/code.php';
 	return runCode();
     }
-
-    /**
-     */
     public function setval() {
 	require_once 'shorten/lib/setval.php';
 	return setAdminVal();
